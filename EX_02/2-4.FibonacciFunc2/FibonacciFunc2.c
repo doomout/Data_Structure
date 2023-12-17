26e6a0a95e18430dca70fc08b48a4fdc8f3ce630
khg <winquake3@hanmail.net>
2023-12-17 21:34:29 +0900

    2-4.재귀 함수화

---
+++ EX_02/2-4.FibonacciFunc2/FibonacciFunc2.c
@@ -0,0 +1,21 @@
+//2-3. 재귀로 피보나치 구현
+#include	<stdio.h>
+
+int Fibo(int n)
+{
+	printf("func call param %d\n", n);
+
+	if (n == 1)
+		return 0;
+	else if (n == 2)
+		return 1;
+	else
+		return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int main(void)
+{
+	Fibo(7);
+
+	return 0;
+}
